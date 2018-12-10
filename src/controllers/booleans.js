272c1474e78ca7e5a883cae161d41dd839eece30
Jonny Edge <jonnyedge@outlook.com>
2018-12-10 21:14:04 +0000

    Fixed previously erroring Boolean negate test. Will resume working on remaining Boolean tests.

--- src/controllers/booleans.js
+++ src/controllers/booleans.js
@@ -1,10 +1,22 @@
 const { negate } = require('../lib/booleans');
-// const { truthiness } = require('../lib/booleans');
-// const { isOdd } = require('../lib/booleans');
-// const { startsWith } = require('../lib/booleans');
+const { truthiness } = require('../lib/booleans');
+const { isOdd } = require('../lib/booleans');
+const { startsWith } = require('../lib/booleans');
 
 exports.negate = (request, response) => {
-  response.json({ result: negate(request.params.string) });
+  response.json({ result: negate(request.body.value) });
+};
+
+exports.truthiness = (request, response) => {
+  response.json({ result: truthiness(request.params.string) });
+};
+
+exports.isOdd = (request, response) => {
+  response.json({ result: isOdd(request.body.a) });
+};
+
+exports.startsWith = (request, response) => {
+  response.json({ result: startsWith(request.body.a) });
 };
 
 module.exports = exports;
--- src/routes/booleans.js
+++ src/routes/booleans.js
@@ -4,5 +4,8 @@ const booleanController = require('../controllers/booleans');
 const router = express.Router();
 
 router.post('/negate', booleanController.negate);
+router.post('/truthiness', booleanController.truthiness);
+router.get('/is-odd/:a', booleanController.isOdd);
+router.get('/:a/starts-with/:b', booleanController.startsWith);
 
 module.exports = router;
