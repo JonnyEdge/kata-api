924be8ad36752cdb5df0e1ad80eb63e44eefc21b
Jonny Edge <jonnyedge@outlook.com>
2018-12-11 13:20:09 +0000

    Boolean tests completed. Moving to Array tests.

--- src/controllers/booleans.js
+++ src/controllers/booleans.js
@@ -3,20 +3,32 @@ const { truthiness } = require('../lib/booleans');
 const { isOdd } = require('../lib/booleans');
 const { startsWith } = require('../lib/booleans');
 
+const isNumeric = (string) => {
+  return !Number.isNaN(parseInt(string));
+};
+
 exports.negate = (request, response) => {
   response.json({ result: negate(request.body.value) });
 };
 
 exports.truthiness = (request, response) => {
-  response.json({ result: truthiness(request.params.string) });
+  response.json({ result: truthiness(request.body.value) });
 };
 
 exports.isOdd = (request, response) => {
-  response.json({ result: isOdd(request.body.a) });
+  if (isNumeric(request.params.a)) {
+    response.status(200).json({ result: isOdd(request.params.a) });
+  } else {
+    response.status(400).json({ error: 'Parameter must be a number.' });
+  }
 };
 
 exports.startsWith = (request, response) => {
-  response.json({ result: startsWith(request.body.a) });
+  if (((request.params.b).length) === 1) {
+    response.json({ result: startsWith(request.params.b, request.params.a) });
+  } else {
+    response.status(400).json({ error: 'Parameter "character" must be a single character.' });
+  }
 };
 
 module.exports = exports;
